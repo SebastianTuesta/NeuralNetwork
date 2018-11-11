da35314f3e2ce222f8bbe6503fee6aed9bc10982
Sebastian Tuesta <tuestagabo@gmail.com>
2018-11-11 01:48:28 -0500

    Algorithm for learning simple layer perceptron

---
+++ perceptronTrain.m
@@ -0,0 +1,52 @@
+function W0_W = perceptronTrain (n, W0, W, X0, S, T, iter)
+  %% Parameters
+  % n = rate learning
+  % W0 = bias
+  % W = neuron weights
+  % X0 = -1
+  % S = input values trainning
+  % T = output values trainning
+  % iter = max number of iterations
+ 
+  converged = false;
+    
+  for k = 1:iter
+    if converged
+      break;
+    endif
+    
+    converged = true;
+    
+    
+      
+    %Number of Input Unit
+    for i = 1: length(S)
+      X = [X0, S(i,:)];
+      
+      U = [W0 W]*X';
+     
+      Y = arrayfun(@sign, U );
+       
+      for j = 1:length(Y)
+        if  Y(j) != T(i,j)
+          W = W + n*(T(i)- Y(j))*S(i,:);
+          W0 = W0 + n*(T(i)- Y(j))*X0;
+          converged = converged & false;
+        endif   
+      endfor   
+          
+    endfor
+  endfor 
+  
+  
+  W0_W = [W0 W];
+  
+endfunction
+
+function y = sign(u)
+    if (u>=0)
+      y = 1;
+    else
+      y = 0;
+    endif
+endfunction
